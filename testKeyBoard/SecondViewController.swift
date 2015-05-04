23aafb9794615bbf1f18daa3c097d9b89f842d77
BestKora <developer@bestkora.com>
2015-05-04 22:48:04 +0300

    Gesture Recognizer

---
+++ testKeyBoard/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  testKeyBoard
+//
+//  Created by Tatiana Kornilova on 5/4/15.
+//  Copyright (c) 2015 Tatiana Kornilova. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+//    @IBOutlet weak var textField: UITextField!
+    var keyboardDismissTapGesture: UIGestureRecognizer?
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if keyboardDismissTapGesture == nil
+        {
+            keyboardDismissTapGesture = UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard:"))
+            self.view.addGestureRecognizer(keyboardDismissTapGesture!)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if keyboardDismissTapGesture != nil
+        {
+            self.view.removeGestureRecognizer(keyboardDismissTapGesture!)
+            keyboardDismissTapGesture = nil
+        }
+    }
+    
+    func dismissKeyboard(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
+    }
+
+
+}
