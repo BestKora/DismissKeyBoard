39855a5cb52eeb9694226042744ace2790585cae
BestKora <developer@bestkora.com>
2015-05-06 00:21:11 +0300

    Комментарии

--- testKeyBoard/SecondViewController.swift
+++ testKeyBoard/SecondViewController.swift
@@ -11,31 +11,49 @@ import UIKit
 class SecondViewController: UIViewController {
     
 //----- клавиатура скрывается (keyboard DisMiss) начало
-    
-    var keyboardDismissTapGesture: UIGestureRecognizer?
-    
+  
+// Добавляем "радиостанции" для появления клавиатуры и ее "ухода" с экрана
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        NSNotificationCenter.defaultCenter().addObserver(self,
-            selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(
+                                                self,
+                                                selector: Selector("keyboardWillShow:"),
+                                                name: UIKeyboardWillShowNotification,
+                                                object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(
+                                                self,
+                                                selector: Selector("keyboardWillHide:"),
+                                                name:UIKeyboardWillHideNotification,
+                                                object: nil)
     }
     
+// Убираем все "радиостанции"
     override func viewWillDisappear(animated: Bool) {
         NSNotificationCenter.defaultCenter().removeObserver(self)
         
         super.viewWillDisappear(animated)
     }
     
+    var keyboardDismissTapGesture: UIGestureRecognizer?
+    
+// КЛАВИАТУРА ПОЯВИЛАСЬ - добавляем "распознаватель жестов"
     func keyboardWillShow(notification: NSNotification) {
         if keyboardDismissTapGesture == nil
         {
-            keyboardDismissTapGesture = UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard:"))
+            keyboardDismissTapGesture = UITapGestureRecognizer(
+                                          target: self,
+                                          action: Selector("dismissKeyboard:"))
+            keyboardDismissTapGesture?.cancelsTouchesInView = false
             self.view.addGestureRecognizer(keyboardDismissTapGesture!)
         }
     }
     
+    func dismissKeyboard(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+// КЛАВИАТУРА СКРЫЛАСЬ - убираем "распознаватель жестов"
     func keyboardWillHide(notification: NSNotification) {
         if keyboardDismissTapGesture != nil
         {
@@ -43,11 +61,6 @@ class SecondViewController: UIViewController {
             keyboardDismissTapGesture = nil
         }
     }
-    
-    func dismissKeyboard(sender: UITapGestureRecognizer) {
-        view.endEditing(true)
-        
-    }
-    //----- клавиатура скрывается (keyboard DisMiss) конец
+//----- клавиатура скрывается (keyboard DisMiss) конец
 
 }
